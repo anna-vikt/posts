30822a4a0e57821915221e68621339bbe268c9c1
Anna <anna.kirilicheva91@gmail.com>
2023-03-24 14:15:16 +0300

    Вносит правки в api

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -3,6 +3,7 @@ import { Container } from "@mui/system";
 import { postData } from "../../postData";
 import { AppHeader } from "../app-header";
 import { PostList } from "../post-list/post-list";
+import api from "../../utils/api";
 
 
 export function App() {
