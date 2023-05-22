70f38749d39513524a590118cb649fcb9b7c1d4a
Alexana7 <ngayane@bk.ru>
2023-05-22 17:22:07 +0300

    fix create-post-form

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -2,7 +2,7 @@ import s from "./styles.module.css"
 
 function Form( {handleFormSubmit, title, children} ) {
     return (
-        <form className={s.form} onSubmit={handleFormSubmit}>
+        <form noValidate className={s.form} onSubmit={handleFormSubmit}>
             {title && <h3 className={s.title}>{title}</h3>}
             {children}
         </form>
